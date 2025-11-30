5cad4786fa564936a69b40b917a77f4946233aee
Legendyboi <legendyboi.dev@proton.me>
2025-11-30 19:26:49 +0530

    Initial project setup

---
+++ src/client/java/net/vulkanshaders/client/VulkanShadersInitClient.java
@@ -0,0 +1,10 @@
+package net.vulkanshaders.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VulkanShadersInitClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/vulkanshaders/VulkanShadersInit.java
@@ -0,0 +1,10 @@
+package net.vulkanshaders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VulkanShadersInit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
